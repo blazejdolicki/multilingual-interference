b7d5fcb428ae57c5b53a147e57b921bd6058a0d8
bmanczak <manczakb@gmail.com>
2021-05-20 10:15:32 +0200

    Changed break to continue

--- metalearning/train_meta.py
+++ metalearning/train_meta.py
@@ -249,7 +249,7 @@ def main():
             except RuntimeError:
                 print(f'[ERROR]: Encountered a runtime error at iteration {iteration} for training task {j}.',
                       f' Skipping this training task.')
-                break
+                continue
 
         if (iteration + 1) % args.save_every == 0:
             epi_grads = np.array(episode_grads)
